6de471c2458571f30a2e30433ee5ac0e567632df
Harishwar28 <119032255+Harishwar28@users.noreply.github.com>
2024-06-01 21:46:00 +0530

    Add files via upload

---
+++ navalvessel/Vessel.java
@@ -0,0 +1,67 @@
+package Main;
+
+
+public class Vessel {
+	private int vesselId;
+    private String vesselName;
+    private int noOfVoyagesPlanned;
+    private int noOfVoyagesCompleted;
+    private String purpose;
+    private String classification;
+
+    public Vessel(int vesselId, String vesselName, int noOfVoyagesPlanned, int noOfVoyagesCompleted, String purpose) {
+        this.vesselId = vesselId;
+        this.vesselName = vesselName;
+        this.noOfVoyagesPlanned = noOfVoyagesPlanned;
+        this.noOfVoyagesCompleted = noOfVoyagesCompleted;
+        this.purpose = purpose;
+    }
+
+    public int getVesselId() {
+        return vesselId;
+    }
+
+    public void setVesselId(int vesselId) {
+        this.vesselId = vesselId;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public int getNoOfVoyagesPlanned() {
+        return noOfVoyagesPlanned;
+    }
+
+    public void setNoOfVoyagesPlanned(int noOfVoyagesPlanned) {
+        this.noOfVoyagesPlanned = noOfVoyagesPlanned;
+    }
+
+    public int getNoOfVoyagesCompleted() {
+        return noOfVoyagesCompleted;
+    }
+
+    public void setNoOfVoyagesCompleted(int noOfVoyagesCompleted) {
+        this.noOfVoyagesCompleted = noOfVoyagesCompleted;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+}
